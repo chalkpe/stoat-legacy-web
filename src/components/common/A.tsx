b5dbd2ceeedaac94802b8ad8392b44fc2dc8e633
Chalk <9481405+chalkpe@users.noreply.github.com>
2025-10-28 02:13:55 +0900

    feat: download

--- src/components/common/A.tsx
+++ src/components/common/A.tsx
@@ -5,18 +5,18 @@ type Props = Omit<React.ComponentProps<'a'>, 'href' | 'onClick'> & {
     onClick?: () => void;
 };
 
-export const A: React.FC<Props> = ({ href, onClick, children, ...props }) => {
+export const A: React.FC<Props> = ({ href, download, onClick, children, ...props }) => {
     const handleClick = useCallback(() => {
         if ('webkit' in window) {
             const webkit = window.webkit as any;
-            webkit.messageHandlers.windowOpenHandler.postMessage({ url: href });
+            webkit.messageHandlers.windowOpenHandler.postMessage({ url: href, download });
         } else {
             onClick?.();
         }
     }, [href, onClick]);
 
     return (
-        <a href={href} {...props} onClick={handleClick}>
+        <a {...props} href={href} download={download} onClick={handleClick}>
             {children}
         </a>
     )

1230e99bb75faa517871242703cece7c08176964
Chalk <chalk@chalk.pe>
2025-10-28 22:28:30 +0900

    feat: remove backslash

--- src/components/markdown/plugins/anchors.tsx
+++ src/components/markdown/plugins/anchors.tsx
@@ -4,20 +4,27 @@ import { determineLink } from "../../../lib/links";
 
 import { modalController } from "../../../controllers/modals/ModalController";
 
+const replace = (str?: unknown): string | undefined => (typeof str === "string" ? str.replace(/%5C/g, "").replace(/\\/g, "") : undefined);
+
 export function RenderAnchor({
-    href,
+    href: _href,
+    children: _children,
     ...props
 }: JSX.HTMLAttributes<HTMLAnchorElement>) {
+    // Unescape markdown escaped characters
+    const href = replace(_href);
+    const children = Array.isArray(_children) ? _children.map(replace) : replace(_children);
+
     // Pass-through no href or if anchor
-    if (!href || href.startsWith("#")) return <a href={href} {...props} />;
+    if (!href || href.startsWith("#")) return <a href={href} children={children} {...props} />;
 
     // Determine type of link
     const link = determineLink(href);
-    if (link.type === "none") return <a {...props} />;
+    if (link.type === "none") return <a children={children} {...props} />;
 
     // Render direct link if internal
     if (link.type === "navigate") {
-        return <Link to={link.path} children={props.children} />;
+        return <Link to={link.path} children={children} />;
     }
 
     return (
@@ -33,6 +40,7 @@ export function RenderAnchor({
                     ev.currentTarget.innerText !== href,
                 ) && ev.preventDefault()
             }
+            children={children}
         />
     );
 }

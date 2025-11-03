0fe3619264b7ad8248f746391d838b0438a502fa
Chalk <9481405+chalkpe@users.noreply.github.com>
2025-11-04 02:18:22 +0900

    feat: image viewer

--- src/components/common/messaging/attachments/ImageFile.tsx
+++ src/components/common/messaging/attachments/ImageFile.tsx
@@ -1,11 +1,23 @@
 import { API } from "revolt.js";
 
+import LightGallery from 'lightgallery/react';
+import lgRotate from 'lightgallery/plugins/rotate';
+import lgVideo from 'lightgallery/plugins/video';
+import lgZoom from 'lightgallery/plugins/zoom';
+import type { LightGallery as LG } from "lightgallery/lightgallery";
+
+import 'lightgallery/scss/lightgallery.scss';
+import 'lightgallery/scss/lg-rotate.scss';
+import 'lightgallery/scss/lg-video.scss';
+import 'lightgallery/scss/lg-zoom.scss';
+
 import styles from "./Attachment.module.scss";
 import classNames from "classnames";
-import { useState } from "preact/hooks";
+
+import { memo } from "preact/compat";
+import { useRef, useState } from "preact/hooks";
 
 import { useClient } from "../../../../controllers/client/ClientController";
-import { modalController } from "../../../../controllers/modals/ModalController";
 
 enum ImageLoadingState {
     Loading,
@@ -17,26 +29,36 @@ type Props = JSX.HTMLAttributes<HTMLImageElement> & {
     attachment: API.File;
 };
 
-export default function ImageFile({ attachment, ...props }: Props) {
+function ImageFile({ attachment, ...props }: Props) {
     const [loading, setLoading] = useState(ImageLoadingState.Loading);
     const client = useClient();
     const url = client.generateFileURL(attachment)!;
+    const ref = useRef<LG | null>(null);
 
     return (
-        <img
-            {...props}
-            src={url}
-            alt={attachment.filename}
-            loading="lazy"
-            className={classNames(styles.image, {
-                [styles.loading]: loading !== ImageLoadingState.Loaded,
-            })}
-            onClick={() =>
-                modalController.push({ type: "image_viewer", attachment })
-            }
-            onMouseDown={(ev) => ev.button === 1 && window.open(url, "_blank")}
-            onLoad={() => setLoading(ImageLoadingState.Loaded)}
-            onError={() => setLoading(ImageLoadingState.Error)}
-        />
+        <LightGallery 
+            licenseKey="GPLv3"
+            plugins={[lgRotate, lgVideo, lgZoom]}
+            onInit={({ instance }) => ref.current = instance}
+            counter={false}
+            dynamic
+            dynamicEl={[{ src: url, thumb: url, subHtml: `<h4>${attachment.filename}</h4>` }]}
+        > 
+            <img
+                {...props}
+                src={url}
+                alt={attachment.filename}
+                loading="lazy"
+                className={classNames(styles.image, {
+                    [styles.loading]: loading !== ImageLoadingState.Loaded,
+                })}
+                onClick={() => ref.current?.openGallery()}
+                onMouseDown={(ev) => ev.button === 1 && window.open(url, "_blank")}
+                onLoad={() => setLoading(ImageLoadingState.Loaded)}
+                onError={() => setLoading(ImageLoadingState.Error)}
+            />
+        </LightGallery>
     );
 }
+
+export default memo(ImageFile);
